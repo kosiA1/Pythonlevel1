6d0c78c2fbc1ba0d895d721d50e7ca32e3f9a6a4
kosi <anadoziekosiso@gmail.com>
2020-12-05 23:05:03 -0700

    Create Practice 1.py

---
+++ Practice 1.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:       Kosisochi Anadozie
+# Purpose:      practice
+# Created:     05-12-2020
+# Copyright:   (c) anado 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("My first name is kosisochi")
+print("\n")
+print("my last name is Anadozie")
+print("My faculty is Sciences")
+print("My year is 1")
+
+
